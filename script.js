9ad0adb8400f6e9b5a478f3bfb5c5861b9951589
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2023-02-27 20:04:54 +0200

    second

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict"; 
 import { data } from "./data.js";
-
 const container = document.querySelector(".container");
 
 for (let i = 0; i < data.length; i++)  {
@@ -9,78 +8,52 @@ for (let i = 0; i < data.length; i++)  {
         <div class="language">
         <ul class="language_list">
         <span>${data[i].language}</span>
-        <li 
-        style = "
-        color:white; 
-        list-style-type:none; 
+        <li style = "
         background-color: rgb(129, 182, 213); 
-        width:${Math.floor(data[i].popularity*32)}px
-        "> 
+        width:${Math.floor(data[i].popularity*32)}px"> 
         ${data[i].popularity} 
-        </li>
-        </ul>
-    </div>
-        `
+        </li></ul>
+    </div>`
     }
     else if (data[i].popularity > 7 && data[i].popularity <= 13) {
         container.innerHTML += `
         <div class="language">
         <ul class="language_list">
         <span>${data[i].language}</span>
-        <li 
-        style = "
-        color:white; 
-        list-style-type:none; 
+        <li style = "
         background-color: rgb(21, 205, 202);
-        width:${Math.floor(data[i].popularity*32)}px
-        "> 
+        width:${Math.floor(data[i].popularity*32)}px"> 
         ${data[i].popularity} 
-        </li>
-        </ul>
-    </div>
-        `
+        </li></ul>
+    </div>`
     }
     else if (data[i].popularity > 1 && data[i].popularity <= 7) {
         container.innerHTML += `
         <div class="language">
         <ul class="language_list">
         <span>${data[i].language}</span>
-        <li 
-        style = "
-        color:white; 
-        list-style-type:none; 
+        <li style = "
         background-color: rgb(21, 205, 202);
-        width:${Math.floor(data[i].popularity*32)}px
-        "> 
+        width:${Math.floor(data[i].popularity*32)}px"> 
         ${data[i].popularity} 
-        </li>
-        </ul>
-    </div>
-        `
+        </li></ul>
+    </div>`
     }
     else if (data[i].popularity > 0 && data[i].popularity <= 1) {
         container.innerHTML += `
         <div class="language">
         <ul class="language_list">
         <span>${data[i].language}</span>
-        <li 
-        style = "
-        color:white; 
-        list-style-type:none; 
+        <li style = "
         background-color: rgb(190, 232, 232);        
-        width:${Math.floor(data[i].popularity*32)}px
-        "> 
-        </li>
+        width:${Math.floor(data[i].popularity*32)}px"></li>
         <span style = "
         text-align: left;
         color: aqua;
-        font-size: 18px;
         margin-top: 5px;
-        font-weight: bold;
-        "
+        font-weight: bold;"
         >${data[i].popularity} </span>
         </ul>
-    </div>
-        `
+    </div>`
     }
     }
